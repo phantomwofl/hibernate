c2879e26ef93ae82dfc88c287e69ab93d2bb8d7c
phantomwofl <phantomwolf@yandex.ru>
2022-07-15 21:50:37 +0400

    ver1.01

--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -22,50 +22,11 @@ import java.util.stream.Stream;
 @Transactional
 public class Repository {
 
-//    @PersistenceContext
-//    private EntityManager entityManager;
-
     @Autowired
     private CityRepository cityRepository;
 
     @Autowired
     private PersonRepository personRepository;
-
-   // @Transactional
-//    public void setEntityManager() {
-//        var names = List.of("Вася", "Петя", "Коля", "Саша");
-//
-//        var surnames = List.of("Иванов", "Петров", "Сидоров", "Смирнов");
-//        List<City> cities = Stream.of("Москва", "Самара", "Казань", "Астрахань")
-//                        .map(n -> City.builder()
-//                                .name(n)
-//                                .build())
-//                                .collect(Collectors.toUnmodifiableList());
-//        cityRepository.saveAll(cities);
-//
-//        var random = new Random();
-//
-//        IntStream.range(0, 50)
-//                .forEach(i -> {
-//                    var person = Person.builder()
-//                            .name(names.get(random.nextInt(names.size())))
-//                            .surname(surnames.get(random.nextInt(surnames.size())))
-//                            .age(random.nextInt(50))
-//                            .phone_number("4124125")
-//                            .city(cities.get(random.nextInt(cities.size())))
-//                            .build();
-//                    personRepository.save(person);
-//                });
-//    }
-//    @Transactional
-//    public List<Person> getPersonsByCity (String city) {
-//        setEntityManager();
-//        List<Person> persons = null;
-//        Query query = entityManager.createQuery("select p from Person p where p.city = :city");
-//        query.setParameter("city", city);
-//        query.getResultList().containsAll(persons);
-//        return persons;
-//    }
 
     @Transactional
     public List<Person> getPersonsByCity(String city) {
