673d99ac67ae4ff97292885f0ddd8bf4465ab6fc
phantomwofl <phantomwolf@yandex.ru>
2022-08-02 14:52:45 +0400

    ver1.01

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,11 +1,14 @@
 package controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import services.Service;
 import entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @org.springframework.stereotype.Controller
@@ -14,18 +17,27 @@ public class Controller {
     @Autowired
     private Service service = new Service();
 
+    @Secured({"READ"})
     @GetMapping("/persons/by-city")
     public List<Person> getPersonsByCity(@RequestParam("city") String city) {
         return service.getPersonsByCity(city);
     }
 
+    @PreAuthorize("hasAnyRole('WRITE' or 'DELETE')")
     @GetMapping("/persons/by-age")
     public List<Person> getPersonsByAge(@RequestParam("age") int age) {
         return service.getPersonsByAge(age);
     }
 
+    @RolesAllowed({"WRITE"})
     @GetMapping("/persons/by-name&surname")
     public List<Person> getPersonsByNameAndSurname(@RequestParam("name") String name, @RequestParam("surname") String surname) {
         return service.getPersonsByNameAndSurname(name, surname);
+    }
+
+    @PreAuthorize("#username == authentication.principal.username")
+    @GetMapping("/persons")
+    public String greetingUser(@RequestParam("username") String username) {
+        return "Hello from secure app, " + username;
     }
 }
--- src/main/java/ru/netology/hibernate/SecurityConfiguration.java
+++ src/main/java/ru/netology/hibernate/SecurityConfiguration.java
@@ -1,29 +1,35 @@
 package ru.netology.hibernate;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("Admin").password("{noop}password").authorities("access");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
+                .withUser("Oleg").password(encoder().encode("password"))
+                .roles("READ")
                 .and()
-                .authorizeHttpRequests().antMatchers("/persons/by-city").permitAll()
+                .withUser("Ivan").password(encoder().encode("password2"))
+                .roles("WRITE")
                 .and()
-                .authorizeHttpRequests().antMatchers("/persons/by-age").hasAuthority("access")
-                .and()
-                .authorizeHttpRequests().anyRequest().authenticated();
+                .withUser("Pavel").password(encoder().encode("password3"))
+                .roles("DELETE");
     }
 }
